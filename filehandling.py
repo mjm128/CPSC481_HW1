6cf7a8b5a765dfe7fbad233769a9c189d39c2620
MJM128 <mjm128@gmail.com>
2016-09-23 03:13:02 -0700

    Fixed check4Move so that it handles all corner cases

--- filehandling.py
+++ filehandling.py
@@ -1,23 +1,30 @@
 import time
 import os
 
-def check4Move(player):
+def check4Move(player, moveNum):
+	#MoveNum must start at 1 and go up to N
+	moveList = []
 	if player == "X":
-		file = "log_x.txt"
-	if player == "Y":
 		file = "log_y.txt"
+	if player == "Y":
+		file = "log_x.txt"
 	while True:
-		with open(file, "r") as f:
-			lastLine = ""
+		with open(file, "r") as f:	
+			#Check of file is empty
 			if os.stat(file).st_size == 0:
-				time.sleep(1)
+				time.sleep(2)
 				continue
 			for line in f:
-				lastLine = line.rstrip()
-			if lastLine[0] != player:
+				moveList.append(line.rstrip())
+			if player == "X" and len(moveList) == (moveNum*2):
 				break
-		time.sleep(1)
-	return lastLine
+			if player == "Y" and len(moveList) == (1+((moveNum-1)*2)):
+				print(len(moveList))
+				break
+		#Clear list if move not added yet
+		del moveList[:]
+		time.sleep(2)
+	return moveList[-1]
 	
 	
-print(check4Move("X"))
+print(check4Move("X", 1))
