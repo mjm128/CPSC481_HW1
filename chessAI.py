d162e0cd22a98207cea6a646383b86b412c9fdc4
mjm128 <mjm128@gmail.com>
2016-09-30 20:15:19 -0700

    Alpha Beta Makes Random Move

--- chessAI.py
+++ chessAI.py
@@ -6,8 +6,46 @@ def randomPlayer(board):
 	move = random.choice(list(board.legal_moves))
 	return move.uci()
 	
-def playerX(board):
-	pass
+def computerPlayer(board):
+	#Call to get which move is best
+	board_copy = board
+	moveList = []
+	for i in board_copy.legal_moves:
+		score = alphaBetaMax(board_copy, float("-inf"), float("inf"), 3)
+		moveList.append((i, score))
 	
-def playerY(board):
-	pass
+	
+	return moveList[0][0]
+	
+def alphaBetaMax(board, alpha, beta, depth):
+	if depth == 0:
+		return evaluate(board)
+	
+	#Iterate through legal moves, DFS.
+	for i in board.legal_moves:
+		board.push(i)
+		score = alphaBetaMin(board, alpha, beta, depth-1)
+		board.pop()
+		if score >= beta:
+			return beta
+		if score > alpha:
+			alpha = score
+	return alpha
+	
+def alphaBetaMin(board, alpha, beta, depth):
+	if depth == 0:
+		return evaluate(board)
+		
+	for i in board.legal_moves:
+		board.push(i)
+		score = alphaBetaMax(board, alpha, beta, depth-1)
+		board.pop()
+		if score <= alpha:
+			return alpha
+		if score < beta:
+			beta = score
+	return beta
+	
+def evaluate(board):
+	return random.randrange(0,10000)
+	
