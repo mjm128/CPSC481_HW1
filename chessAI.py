24a5540041b5b97a69f8973595a65d9f05149c11
MJM128 <mjm128@gmail.com>
2016-10-02 21:25:34 -0700

    Fixed depth calculation issue

--- chessAI.py
+++ chessAI.py
@@ -11,15 +11,21 @@ def computerPlayer(board):
 	#Call to get which move is best
 	board_copy = board
 	moveList = []
+	bestMoves = []
 	for i in board_copy.legal_moves:
 		board_copy.push(i)
-		score = alphaBetaMin(board_copy, float("-inf"), float("inf"), 3)
+		score = alphaBetaMin(board_copy, float("-inf"), float("inf"), 5)
 		board_copy.pop()
 		moveList.append((i, score))
 	
-	move = sorted(moveList, key=itemgetter(1), reverse=True)[0][0]
-	print(move)
-	return move
+	#Get the best score from moves
+	bestScore = sorted(moveList, key=itemgetter(1), reverse=True)[0][1]
+	for i in moveList:
+		if i[1] == bestScore:
+			#Add best moves to list
+			bestMoves.append(i[0])
+			
+	return random.choice(bestMoves) #Return random best move
 	
 def alphaBetaMax(board, alpha, beta, depth):
 	if depth == 0 or board.is_checkmate() or board.is_stalemate():
@@ -38,7 +44,7 @@ def alphaBetaMax(board, alpha, beta, depth):
 	
 def alphaBetaMin(board, alpha, beta, depth):
 	if depth == 0 or board.is_checkmate() or board.is_stalemate():
-		return evaluate(board)
+		return -evaluate(board)
 		
 	for i in board.legal_moves:
 		board.push(i)
@@ -60,10 +66,12 @@ def heuristicX(board):
 	score = 0
 	score -= len(board.move_stack) #Number of moves
 	score += 9001 if board.is_checkmate() else 0
+	score += 99 if board.is_check() else 0
+	score -= len(board.legal_moves) * 5
 	
 	for (piece, value) in [ (chess.KING, 0),
-							(chess.ROOK, 50),
-							(chess.KNIGHT, 25)]:
+							(chess.ROOK, 300),
+							(chess.KNIGHT, 100)]:
 		score += len(board.pieces(piece, board.turn)) * value
 		score -= len(board.pieces(piece, not board.turn)) * value
 	return score
@@ -72,10 +80,12 @@ def heuristicY(board):
 	score = 0
 	score -= len(board.move_stack) #Number of moves
 	score += 9001 if board.is_checkmate() else 0
+	score += 99 if board.is_check() else 0
+	score -= len(board.legal_moves) * 5
 	
 	for (piece, value) in [ (chess.KING, 0),
-							(chess.ROOK, 50),
-							(chess.KNIGHT, 25)]:
+							(chess.ROOK, 300),
+							(chess.KNIGHT, 100)]:
 		score += len(board.pieces(piece, board.turn)) * value
 		score -= len(board.pieces(piece, not board.turn)) * value
 	return score
